fdbf52af720e9fca0bebbe78883f2d209cfc00ef
unknown <10433084@CS514-06.ad.uvu.edu>
2016-02-11 19:29:27 -0700

    uploaded my in class work.

---
+++ once again other project/Assets/Move.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	//player movement
+	public float moveSpeed;
+	public float jumpheight;
+
+	//Ground Check
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate() {
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.A)) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if (Input.GetKeyDown (KeyCode.Space)&& grounded) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpheight);
+		}
+
+	}
+}
