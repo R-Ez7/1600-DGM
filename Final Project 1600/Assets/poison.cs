2fdc5600d1372f8a006102b36d41967db5838f2c
unknown <10433084@CS514-06.ad.uvu.edu>
2016-03-31 20:51:09 -0600

    final

---
+++ Final Project 1600/Assets/poison.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class poison : MonoBehaviour {
+
+	public int pointsToSubtract;
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.GetComponent<Move> () == null)
+			return;
+		ScoreManager.SubtractPoints (pointsToSubtract);
+
+		//This will distroy the coin once collected
+		Destroy (gameObject);
+
+	}
+
+}
---
+++ once again other project/Assets/pickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickup : MonoBehaviour {
+
+	public int pointsToAdd;
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.GetComponent<Move> () == null)
+			return;
+		ScoreManager.AddPoints (pointsToAdd);
+
+		//This will distroy the coin once collected
+		Destroy (gameObject);
+
+	}
+
+}
